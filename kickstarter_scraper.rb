5aac65d5755edfc7c71df46d941069728648773e
79ceca7e-1710-4439-8f8b-c115f316e9e9 <github-enterprise+79ceca7e-1710-4439-8f8b-c115f316e9e9@flatironschool.com>
2020-02-16 23:37:17 -0800

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,36 @@
 # require libraries/modules here
+require 'pry'
+require 'nokogiri'
+
+# :projects => {
+#   "My Great Project"  => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   },
+#   "Another Great Project" => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   }
+# }
 
 def create_project_hash
-  # write your code here
+  
+  html = File.read("./fixtures/kickstarter.html")
+  kickstarter = Nokogiri::HTML(html).css(".project-card")
+  projects = Hash.new({})
+  kickstarter.each do |project|
+    projects[project.css(".bbcard_name strong a").text.strip.to_sym] = {
+      image_link: project.css(".project-thumbnail a img").attribute("src").value.strip,
+      description: project.css(".bbcard_blurb").text.strip,
+      location: project.css(".project-meta .location-name").text.strip,
+      percent_funded: project.css(".project-stats .first strong").text.strip.gsub("%", "").to_i,
+    }
+    
+  end
+  projects
+
 end
